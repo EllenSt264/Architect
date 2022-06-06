05b62cc1cb390dae453b2cc8943b859b204e2363
EllenSt264 <stafford.ellen@protonmail.com>
2022-06-06 16:44:45 +0100

    Implement POST functionality to API

--- api/tests.py
+++ api/tests.py
@@ -11,28 +11,29 @@ def shortenTime(time):
 
 
 class NotesTest(TestCase):
-    def test_get_notes(self):
-        """Test the GET request."""
-        # Add first note
+    """Test GET, POST, PUT and DELETE requests for notes API."""
+
+    def test_add_note(self):
+        """Test the POST request for adding new notes."""
         res = self.client.post('/api/notes/', data=json.dumps({
             'body': 'Hello world',
         }),
-            content_type='application/json')
+            content_type='application/json',
+        )
         self.assertEquals(res.status_code, 201)
-        id1 = json.loads(res.content)['id']
 
-        # Add second note
-        res = self.client.post('/api/notes/', data=json.dumps({
-            'body': 'Lorem ipsum...',
-        }),
-            content_type='application/json')
-        self.assertEquals(res.status_code, 201)
-        id2 = json.loads(res.content)['id']
-
-        # Test GET request for both items
-        res = self.client.get('/api/notes/', content_type='application/json')
-        self.assertEquals(res.status_code, 200)
         result = json.loads(res.content)
-        self.assertEquals(len(result), 2)
-        self.assertTrue(result[0]['id'] == id1 or result[1]['id'] == id1)
-        self.assertTrue(result[0]['id'] == id2 or result[1]['id'] == id2)
+        self.assertEquals(result['body'], 'Hello world')
+        self.assertEquals(shortenTime(result['updated']), shortenTime(
+            datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%fZ')))
+        self.assertEquals(shortenTime(result['created_at']), shortenTime(
+            datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%fZ')))
+
+    def test_add_note_invalid(self):
+        """Do not allow notes with invalid data to be created."""
+        res = self.client.post('/api/notes/', data=json.dumps({
+            'body': '',
+        }),
+            content_type='application/json',
+        )
+        self.assertEquals(res.status_code, 400)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+import json
 
-# Create your views here.
+from .models import Note
+from .serializers import NoteSerializer
+
+
+@api_view(['POST'])
+def getNotes(request):
+    """Get notes and create new ones."""
+    if request.method == 'POST':
+        data = request.data
+
+        if data['body'] and not data['body'].isspace():
+            note = Note.objects.create(body=data['body'])
+            # notes must be serialized before passing it into the response
+            # # so that json can read it
+            serializer = NoteSerializer(instance=note, data=data, many=False)
+
+            if serializer.is_valid():
+                serializer.save()
+                # serializer is an object so we use '.data' to access it
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response('Invalid data', status=status.HTTP_400_BAD_REQUEST)
+
+    # Request method error
+    return HttpResponse(json.dumps({'detail': 'Wrong method'}),
+                        status=status.HTTP_501_NOT_IMPLEMENTED)
